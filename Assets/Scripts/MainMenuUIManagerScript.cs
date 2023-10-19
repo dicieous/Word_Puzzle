b3d24ac5961fcd4616ffe7b19f40eec721baebc2
Happy Singh <happyarnav425@gmail.com>
2023-10-19 17:52:39 +0530

    Fixed Some More Camera Angles

---
+++ Assets/Scripts/MainMenuUIManagerScript.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUIManagerScript : MonoBehaviour
+{
+    [SerializeField] private ScrollRect levelsScrollRect;
+
+    public static MainMenuUIManagerScript Instance;
+    
+    
+    
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void MoveToActivePlayableLevel()
+    {
+        Debug.Log("3");
+        var levelImageIndex = PlayerPrefs.GetInt("Level");
+
+        VerticalLayoutGroup vert = levelsScrollRect.content.GetComponent<VerticalLayoutGroup>();
+        levelImageIndex = Mathf.Clamp(levelImageIndex, 0, levelsScrollRect.content.transform.childCount-1);
+
+        float scrollHeight = 0f; 
+        for (int i = 0; i < levelImageIndex - 2; i++)
+        {
+            var childTransform = vert.transform.GetChild(i) as RectTransform;
+            if (childTransform != null) scrollHeight += childTransform.rect.height + vert.spacing;
+        }
+
+        var viewportHeight = levelsScrollRect.viewport.rect.height;
+
+        var normalizedPosition = scrollHeight / (levelsScrollRect.content.rect.height - viewportHeight);
+        normalizedPosition = Mathf.Clamp01(normalizedPosition);
+        levelsScrollRect.verticalNormalizedPosition = normalizedPosition;
+        
+        Debug.Log("scrollHeight: " + scrollHeight);
+        Debug.Log("viewportHeight: " + viewportHeight);
+        Debug.Log("normalizedPosition: " + normalizedPosition);
+
+    }
+}
