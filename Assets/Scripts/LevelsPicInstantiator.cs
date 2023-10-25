5c8150000936c72afca12e0cdafad408b9df0652
Happy Singh <happyarnav425@gmail.com>
2023-10-25 12:02:57 +0530

    Added AutoComplete and Map(Some Functionalities)

--- Assets/Scripts/LevelsPicInstantiator.cs
+++ Assets/Scripts/LevelsPicInstantiator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,13 +9,30 @@ using UnityEngine.UI;
 
 public class LevelsPicInstantiator : MonoBehaviour
 {
-
     [SerializeField] private GameObject levelImages;
-    
-    
+
+
     void Start()
     {
+        SetContentHeight();
         SetLevelImages();
+    }
+    
+
+    private void SetContentHeight()
+    {
+        var totalContentCount = SceneManager.sceneCountInBuildSettings - 2;
+
+        var vert = GetComponent<VerticalLayoutGroup>();
+        var child = levelImages.transform as RectTransform;
+
+        float scrollHeight = 0f;
+        if (child != null) scrollHeight = (child.rect.height + vert.spacing) * (totalContentCount);
+
+        var rect = GetComponent<RectTransform>().sizeDelta;
+        GetComponent<RectTransform>().sizeDelta =  new Vector2(rect.x, scrollHeight);
+        Debug.Log("ScrollHeight of Objects " + scrollHeight);
+        Debug.Log("rectHeight of Objects " + rect.y);
     }
 
     //To show the level Which you need to play now
@@ -22,13 +40,13 @@ public class LevelsPicInstantiator : MonoBehaviour
     {
         Debug.Log("2");
         var activeLevel = PlayerPrefs.GetInt("Level");
-        Debug.Log("Active Level " + activeLevel);
-        
+        //Debug.Log("Active Level " + activeLevel);
+
         var child = transform.GetChild(activeLevel);
-       
-        
-        if(child!=null) child.GetComponent<Image>().color = Color.blue;
-        
+
+
+        if (child != null) child.GetComponent<Image>().color = Color.blue;
+
         MainMenuUIManagerScript.Instance.MoveToActivePlayableLevel();
     }
 
@@ -36,17 +54,16 @@ public class LevelsPicInstantiator : MonoBehaviour
     void SetLevelImages()
     {
         Debug.Log("1");
-        var imagesNoToInstantiate = SceneManager.sceneCountInBuildSettings - 1;
+        var imagesNoToInstantiate = SceneManager.sceneCountInBuildSettings - 2;
         //Debug.Log("StartCalled " + imagesNoToInstantiate);
         for (int i = 0; i < imagesNoToInstantiate; i++)
         {
             if (this == null) continue;
-            var image  = Instantiate(levelImages, transform, true);
-            image.GetComponentInChildren<TextMeshProUGUI>().text = i.ToString();
+            var image = Instantiate(levelImages, transform, true);
+            image.GetComponentInChildren<TextMeshProUGUI>().text = (i+1).ToString();
             //Debug.Log("Called");
         }
-        
+
         ShowTheActiveLevel();
     }
-    
 }
